bd1ef655bf5bdf7800c15b58e81baedbee8377f2
Bassam Mohammed <bassam7e@gmail.com>
2023-08-12 19:55:12 +0300

    edit

--- lib/layout/screens/favorite.dart
+++ lib/layout/screens/favorite.dart
@@ -1,10 +1,169 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:anamil/constants/MyColors.dart';
+import 'package:anamil/constants/images.dart';
+import 'package:anamil/layout/widgets/productBox.dart';
 import 'package:flutter/material.dart';
 
-class Favorite extends StatelessWidget {
+var products = [
+  ProductBox(
+    product: 'لوحة تشكيلية',
+    desc: 'باستخدام الألوان الزيتية مع لوح...',
+    image: Images.imageDemo1,
+    price: 218,
+  ),
+  ProductBox(
+    product: 'لوحة تشكيلية',
+    desc: 'باستخدام الألوان الزيتية مع لوح...',
+    image: Images.imageDemo1,
+    price: 218,
+  ),
+  ProductBox(
+    product: 'لوحة تشكيلية',
+    desc: 'باستخدام الألوان الزيتية مع لوح...',
+    image: Images.imageDemo1,
+    price: 218,
+  ),
+  ProductBox(
+    product: 'لوحة تشكيلية',
+    desc: 'باستخدام الألوان الزيتية مع لوح...',
+    image: Images.imageDemo1,
+    price: 218,
+  ),
+  ProductBox(
+    product: 'لوحة تشكيلية',
+    desc: 'باستخدام الألوان الزيتية مع لوح...',
+    image: Images.imageDemo1,
+    price: 218,
+  ),
+  ProductBox(
+    product: 'لوحة تشكيلية',
+    desc: 'باستخدام الألوان الزيتية مع لوح...',
+    image: Images.imageDemo1,
+    price: 218,
+  ),
+  ProductBox(
+    product: 'لوحة تشكيلية',
+    desc: 'باستخدام الألوان الزيتية مع لوح...',
+    image: Images.imageDemo1,
+    price: 218,
+  ),
+  ProductBox(
+    product: 'لوحة تشكيلية',
+    desc: 'باستخدام الألوان الزيتية مع لوح...',
+    image: Images.imageDemo1,
+    price: 218,
+  ),
+];
+String dropdownValue = 'المفضلة';
+
+class Favorite extends StatefulWidget {
   const Favorite({super.key});
 
   @override
+  State<Favorite> createState() => _FavoriteState();
+}
+
+class _FavoriteState extends State<Favorite> {
+  @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Favorite'));
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'المفضلة',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+          ),
+        ),
+        automaticallyImplyLeading: false,
+        backgroundColor: MyColors.blue,
+        elevation: 0,
+      ),
+      body: Column(
+        children: [
+          appbarFavoriteWidget(context),
+          Expanded(
+            child: productsGridView(context),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget productsGridView(BuildContext context) {
+    return GridView(
+      padding: EdgeInsets.all(10),
+      physics: BouncingScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: MediaQuery.of(context).size.width / 2,
+        mainAxisExtent: MediaQuery.of(context).size.height / 3.7,
+        mainAxisSpacing: 5,
+        crossAxisSpacing: 5,
+      ),
+      children: products,
+    );
+  }
+
+  Padding appbarFavoriteWidget(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width / 1.3,
+            height: 42,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(
+                color: Color(0xffCECECE),
+                width: 1,
+              ),
+            ),
+            child: DropdownButton(
+              value: dropdownValue,
+              icon: SizedBox(),
+              elevation: 2,
+              underline: SizedBox(),
+              items: ['المفضلة', 'لوحات', 'لوحات زيتية', 'خزف']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                    child: Text(
+                      value,
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
+                    ),
+                  ),
+                );
+              }).toList(),
+              onChanged: (String? newValue) {
+                setState(() {
+                  dropdownValue = newValue!;
+                });
+              },
+            ),
+          ),
+          Container(
+            width: 38,
+            height: 38,
+            decoration: BoxDecoration(
+              color: MyColors.blue,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: IconButton(
+              highlightColor: Colors.transparent,
+              splashColor: Colors.transparent,
+              onPressed: () {
+                print('icon button for filter');
+              },
+              icon: Icon(size: 20, Icons.menu, color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
